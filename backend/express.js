b17c946343223ba9376c649299b5e7610cb6d628
Mohammed Abdul Jawad Vajeeh <51875089+jawadsab@users.noreply.github.com>
2021-11-13 08:03:57 +0530

    backend-skeleton

--- backend/express.js
+++ backend/express.js
@@ -5,6 +5,9 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from './routes/auth.routes'
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -14,6 +17,18 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
 
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }else if (err) {
+    res.status(400).json({"error" : err.name + ": " + err.message})
+    console.log(err)
+  }
+})
+
+
+app.use("/",userRoutes);
+app.use("/",authRoutes);
 
 
 
